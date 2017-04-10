9f8ac49ed5c03a22f85af253d645044ceb5747de
Mike Dongyub Ryu <7imeout@gmail.com>
2017-04-09 21:49:36 -0700

    Setting up default solutions directory (mostly as backup mechanism).

---
+++ generate_solution.m
@@ -0,0 +1,41 @@
+%% init
+clear;
+format short;
+format compact;
+    
+%% working directory is ./solutions/source
+cd('./solutions/source/');
+
+%% get labs to grade (written by Python)
+labs = importdata('./../../submissions/lab_list.dat');
+num_labs = length(labs);
+
+for l = 1:num_labs
+    lab_file_str = char(labs(l));
+    lab_name_str = lab_file_str(1:length(lab_file_str) - 2);
+    
+    fprintf('Genering a solution for: %s ...\n\n', lab_file_str);
+
+    % output filename format is labXX.out.txt
+    output_filename = strcat(lab_name_str, '.out.txt');
+    diary(strcat('../', output_filename));
+
+    % run solution source code and save output
+    try
+        if exist(lab_file_str, 'file') == 2
+            eval(lab_name_str);
+            who;
+        end
+    catch exc
+       report = getReport(exc); 
+       display(report);
+    end
+
+    diary off;
+    
+    % reload lab list as it got cleared by solution
+    labs = importdata('./../../submissions/lab_list.dat');
+end
+
+%% cleanup
+clear;
